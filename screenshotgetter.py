3e0a479a829aa7cc511e6801aaea44fd48f6363c
Skin <Skin@s.local>
2020-06-01 22:53:04 -0400

    added eric's version

---
+++ screenshotgetter.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+from os import system
+from random import randint
+
+# initial url
+url = "https://prnt.sc/"
+
+#add random character to the url r number of times
+for i in range(2):
+    url += chr(randint(
+        ord('a'), ord('z') + 1
+    ))
+
+for j in range(4):
+    url += str(randint(0, 10))
+
+system(f"open {url}")
+system(f"xdg-open {url}")
